fa41ac45b58abb64e3b81b91fcb71f1d13891921
Christian Haitian <christianhaitian0@gmail.com>
2025-05-10 10:42:23 -0400

    Only move compressed xz file if location actually exists

--- create_image.sh
+++ create_image.sh
@@ -3,4 +3,6 @@
 sudo rm -f /media/sf_VMShare/RGB10\ Boot/ArkOS_RGB10.img*
 sudo cp ArkOS_RGB10.img /media/sf_VMShare/RGB10\ Boot/ArkOS_RGB10.img
 xz --keep -z -9 -T0 ArkOS_RGB10.img
-mv ArkOS_RGB10.img.xz /media/sf_VMShare/RGB10\ Boot/
+if [ -d "/media/sf_VMShare" ]; then
+  mv ArkOS_RGB10.img.xz /media/sf_VMShare/RGB10\ Boot/
+fi
