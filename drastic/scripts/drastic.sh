8ba3b847ae5ccada32ef14f5ed173d8e0a0c80f7
Christian Haitian <christianhaitian0@gmail.com>
2026-01-04 12:49:57 -0500

    Add efficiency to the drastic.sh script

--- drastic/scripts/drastic.sh
+++ drastic/scripts/drastic.sh
@@ -2,44 +2,16 @@
 
 directory="$(dirname "$1" | cut -d "/" -f2)"
 
-if  [[ ! -d "/${directory}/nds/backup" ]]; then
-  mkdir /${directory}/nds/backup
-fi
-if  [[ ! -d "/${directory}/nds/cheats" ]]; then
-  mkdir /${directory}/nds/cheats
-fi
-if  [[ ! -d "/${directory}/nds/savestates" ]]; then
-  mkdir /${directory}/nds/savestates
-fi
-if  [[ ! -d "/${directory}/nds/slot2" ]]; then
-  mkdir /${directory}/nds/slot2
-fi
-
-
-if [[ -d "/opt/drastic/backup" && ! -L "/opt/drastic/backup" ]]; then
-  cp -n /opt/drastic/backup/* /${directory}/nds/backup/
-  rm -rf /opt/drastic/backup/
-fi
-
-if [[ -d "/opt/drastic/savestates" && ! -L "/opt/drastic/savestates" ]]; then
-  cp -n /opt/drastic/savestates/* /${directory}/nds/savestates/
-  rm -rf /opt/drastic/savestates/
-fi
-
-if [[ -d "/opt/drastic/cheats" && ! -L "/opt/drastic/cheats" ]]; then
-  cp -n /opt/drastic/cheats/* /${directory}/nds/cheats/
-  rm -rf /opt/drastic/cheats/
-fi
-
-if [[ -d "/opt/drastic/slot2" && ! -L "/opt/drastic/slot2" ]]; then
-  cp -n /opt/drastic/slot2/* /${directory}/nds/slot2/
-  rm -rf /opt/drastic/slot2/
-fi
-
-ln -sf /${directory}/nds/backup /opt/drastic/
-ln -sf /${directory}/nds/cheats /opt/drastic/
-ln -sf /${directory}/nds/savestates /opt/drastic/
-ln -sf /${directory}/nds/slot2 /opt/drastic/
+for d in backup cheats savestates slot2; do
+  if [[ ! -d "/${directory}/nds/$d" ]]; then
+    mkdir /${directory}/nds/${d}
+  fi
+  if [[ -d "/opt/drastic/$d" && ! -L "/opt/drastic/$d" ]]; then
+    cp -n /opt/drastic/${d}/* /${directory}/nds/${d}/
+    rm -rf /opt/drastic/${d}/
+  fi
+  ln -sf /${directory}/nds/${d} /opt/drastic/
+done
 
 echo "VAR=drastic" > /home/ark/.config/KILLIT
 sudo systemctl restart killer_daemon.service
